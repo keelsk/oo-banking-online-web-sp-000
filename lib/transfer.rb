93af57dacc6cdab06dfdcdad448389ccce9955d4
Kirsten Keels <keelsk@gmail.com>
2020-03-06 22:30:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Transfer
   # your code here
   attr_accessor :balance, :bank_account
@@ -40,6 +41,7 @@ class Transfer
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @transfer_status = "complete"
+      binding.pry
     end
   end
   

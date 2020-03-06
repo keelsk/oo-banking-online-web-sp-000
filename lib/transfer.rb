b5d17225acd9b94b7a4853c42a38e8a902edd692
Kirsten Keels <keelsk@gmail.com>
2020-03-06 22:23:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -36,6 +36,9 @@ class Transfer
   def execute_transaction
     if !(@sender.valid?) || (@sender.balance - @amount < 0)
       puts "The transaction rejected. Please check your account balance."
+    else
+      @sender.balance = @sender.balance - @amount
+      @receiver.balance = @receiver.balance + @amount
     end
   end
   

dbe977113bf7856fac8f8fac05e1131e9ce6e9ce
Kirsten Keels <keelsk@gmail.com>
2020-03-06 22:27:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -46,6 +46,7 @@ class Transfer
   def reverse_transfer
       @sender.balance = @sender.balance + @amount
       @receiver.balance = @receiver.balance - @amount
+      @transfer_status = "reversed"
   end
   
 end

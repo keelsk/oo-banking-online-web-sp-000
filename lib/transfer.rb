182fbaa52edbeb9e9a43b9ea0126f8faa874bc18
Kirsten Keels <keelsk@gmail.com>
2020-03-06 22:13:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
   # your code here
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @transfer_status = "pending"
+  end
+  
 end
